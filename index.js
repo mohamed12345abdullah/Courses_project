7a67c1ec4298df8eced208a6ea963d68bbf80309
mohamed12345abdullah <112867836+mohamed12345abdullah@users.noreply.github.com>
2024-10-08 21:46:15 +0300

    Update index.js  cors

--- index.js
+++ index.js
@@ -9,14 +9,7 @@ app.use(bodyParser.json())
 
  
 // CORS middleware
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin',"*" );
-
-    res.header('Access-Control-Allow-Origin', 'no-cors'); // Replace with your client's origin
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+app.use("/",cors());
 
 
 
